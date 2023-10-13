db354cdfff36550f0441777f08e8b45a848ee7a8
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-13 12:15:28 +0300

    done

---
+++ (5)fifth_task.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    long res = 0;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 2 == 0 || i % 3 == 0)
+        {
+            res += i;
+        }
+
+    }
+    cout << res << endl;
+
+    return 0;
+}

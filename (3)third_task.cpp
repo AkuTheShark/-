11774c5d010d59548de3677e2a6c25edcfaf680c
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-13 12:15:11 +0300

    done

---
+++ (3)third_task.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    long res = 0;
+
+    for (int i = 1; i <= 200; i++) 
+    {
+        res += i;
+     
+    }
+    cout << res << endl;
+
+    return 0;
+}

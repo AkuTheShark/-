6595407a878ccb776910a821a01dcb6dd98de4c6
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-13 12:14:43 +0300

    done

---
+++ (1)first_task.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    
+    int s_point = 1;
+    int e_point = 30;
+    int res;
+    
+    for (int i = s_point; i <= e_point; i++) 
+    {
+        if (i % 2 == 0)
+        {
+            cout << "Четные числа от 1 до 30: " << endl;
+            cout << i << endl;
+        }
+
+        /*else 
+        {
+            cout << "Нечетные числа от 1 до 30: " << endl;
+            cout << i << endl;
+        }*/
+    }
+
+    for (int i = s_point; i <= e_point; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << "Нечетные числа от 1 до 30: " << endl;
+            cout << i << endl;
+        }
+
+    }
+
+    return 0;
+}

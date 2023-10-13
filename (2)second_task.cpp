092dbf8812223e3285f68172b6bf287a14b9dd62
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-13 12:14:55 +0300

    done

---
+++ (2)second_task.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    cout << "Таблица умножения: " << endl;
+
+    for (int i = 1; i <= 10; i++) 
+       cout << " 2 * " << i << " = " << 2 * i << endl;
+
+    for (int i = 1; i <= 10; i++)
+        cout << " 3 * " << i << " = " << 3 * i << endl;
+
+    for (int i = 1; i <= 10; i++)    
+        cout << " 4 * " << i << " = " << 4 * i << endl;
+
+    for (int i = 1; i <= 10; i++)
+        cout << " 5 * " << i << " = " << 5 * i << endl;
+
+    for (int i = 1; i <= 10; i++)
+        cout << " 6 * " << i << " = " << 6 * i << endl;
+
+    for (int i = 1; i <= 10; i++)
+        cout << " 7 * " << i << " = " << 7 * i << endl;
+
+    for (int i = 1; i <= 10; i++)
+        cout << " 8 * " << i << " = " << 8 * i << endl;
+
+    for (int i = 1; i <= 10; i++)
+        cout << " 9 * " << i <<" = " << 9 * i << endl;
+
+
+
+    return 0;
+}

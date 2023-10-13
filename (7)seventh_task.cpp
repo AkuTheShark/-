de75d4ecb1ecfd6f0322f89f78d552ffdb15f582
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-13 12:15:34 +0300

    done

---
+++ (7)seventh_task.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    cout << "Введите число: \n";
+    int u_num1;
+    cin >> u_num1;
+
+    for (int i = 1; i < u_num1; i++) 
+    {
+        if (u_num1 % i == 0) 
+        {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
